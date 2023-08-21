8ea7f96b0d353aa7bdeedddd0b9109892bd18cff
vfa-huylv <huylv@vitalify.asia>
2023-08-21 11:26:08 +0700

    #demo8 - build app using BLoC

---
+++ lib/logic/cubit/counter_state.dart
@@ -0,0 +1,12 @@
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'counter_cubit.dart';
+
+class CounterState {
+  int counterValue;
+  bool wasIncremented;
+  CounterState({
+    required this.counterValue,
+    required this.wasIncremented,
+  });
+}
---
+++ lib/presentation/screens/third_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_demo/logic/cubit/counter_cubit.dart';
+
+import 'second_screen.dart';
+
+class ThirdScreen extends StatefulWidget {
+  const ThirdScreen({super.key, required this.title, required this.color});
+
+  final String title;
+  final Color color;
+
+  @override
+  State<ThirdScreen> createState() => _ThirdScreenState();
+}
+
+class _ThirdScreenState extends State<ThirdScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: BlocListener<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.wasIncremented) {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              content: Text('Incremented'),
+              duration: Duration(milliseconds: 300),
+            ));
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              content: Text('Decremented'),
+              duration: Duration(milliseconds: 300),
+            ));
+          }
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('The counter is displayed:'),
+              BlocBuilder<CounterCubit, CounterState>(
+                builder: (context, state) {
+                  return Text(state.counterValue.toString());
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    heroTag: 'btn1',
+                    onPressed: () =>
+                        {BlocProvider.of<CounterCubit>(context).decrement()},
+                    tooltip: 'Decrement',
+                    child: const Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    heroTag: 'btn2',
+                    onPressed: () =>
+                        {BlocProvider.of<CounterCubit>(context).increment()},
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.add),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
